989bcff7c24226f5c4afe46358fe94d685b869ee
KamuiGod <meziani.mk@gmail.com>
2020-04-18 22:29:20 +0100

    Initial author routes

--- routes/products.js
+++ routes/products.js
@@ -1,20 +1,45 @@
 const express = require('express')
 const router = express.Router()
-const Product = require('../models/products')
+const Products = require('../models/products')
 
 // All Products Route
-router.get('/', (req, res) => {
-    res.render('products/index')
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const products = await Products.find(searchOptions)
+        res.render('products/index', { 
+            products: products,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
+    
 })
 
 // New Product Route
 router.get('/new', (req, res) => {
-    res.render('products/new', { product: new Product() })
+    res.render('products/new', { product: new Products() })
 })
 
 // Create Product Route
-router.post('/', (req, res) => {
-    res.send(req.body.name)
+router.post('/', async (req, res) => {
+    const product = new Products({
+        name: req.body.name
+    })
+    try {
+        const newProduct = await product.save()
+        //res.redirect(`products/${newProducts.id}`)
+        res.redirect(`products`)
+    } catch {
+        res.render('products/new', {
+            product: product,
+            errorMessage: "Erreur lors de la création du produit"
+        })
+    }
 })
 
 module.exports = router
